91b4b47141f45b8e0618e10d0b402493396951f8
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-03-05 09:40:41 +0100

    btn changing color if actif

--- devises/views.py
+++ devises/views.py
@@ -5,7 +5,7 @@ import api
 def dashboard(request, user_delta = 60, user_devise="CAD"):
     days, rates = api.get_rates(currencies=user_devise.split(","), days=user_delta)
     days_label = {7: "Semaine",30: "Mois", 365: "Année"}.get(user_delta, "Personalisé")
-    return render(request,'devises/index.html', context={'data': rates,
-                                                         'days_labels': days,
+    return render(request,'devises/index.html', context={'data': rates, # axe y de chart
+                                                         'days_labels': days, # axe x de chart
                                                          'user_devise_btn':user_devise, #for value buttons
-                                                         'days_label': days_label})
+                                                         'days_label': days_label})# titre dynamique
