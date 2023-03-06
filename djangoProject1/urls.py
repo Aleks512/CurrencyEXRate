6db002ade415db20b6e47cd9bad7ebb06ade7d46
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-03-06 11:10:08 +0100

    redirect avec les params

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from devises.views import dashboard
+from devises.views import dashboard, redirect_home
 
 urlpatterns = [
+    path('',redirect_home),
     path('admin/', admin.site.urls),
     path('days=<int:user_delta>&currencies=<str:user_devise>', dashboard, name='dashboard')
 ]
