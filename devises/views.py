57861acdd78df7b2225a3ccd4a20e7f00f2eecd4
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-03-06 13:30:02 +0100

    adding trendline

--- devises/views.py
+++ devises/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import api
 
 # Create your views here.
+def redirect_home(request):
+    return redirect("dashboard", user_delta=30, user_devise="USD,PLN")
+
+
 def dashboard(request, user_delta = 60, user_devise="CAD"):
     days, rates = api.get_rates(currencies=user_devise.split(","), days=user_delta)
     days_label = {7: "Semaine",30: "Mois", 365: "Année"}.get(user_delta, "Personalisé")
